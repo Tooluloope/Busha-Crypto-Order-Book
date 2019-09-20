22d7efd99b03b918f0ef2ece233d11d6a0df1ca0
Adetula Tolulope <adetulatolu@gmail.com>
2019-09-20 07:53:51 +0100

    made column look better

--- src/components/column/column.js
+++ src/components/column/column.js
@@ -1,15 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import'./column.style.css';
 
 export const Column = ({ data, title }) => (
     <div>
-       {/* <div>{title}</div>
-      {data.map(values => (
-        <div className="row">
-          <div>{values[0]}</div>
-          <div>{values[1]}</div>
-        </div>
-    ))}      */}
+       
 
 <table class="table">
   <thead>
